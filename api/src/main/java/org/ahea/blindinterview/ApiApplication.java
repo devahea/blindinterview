aeb6ad144875721288986126509b9f9a41435146
nohsunghyun <nsh1090@brickand.com>
2016-07-21 17:32:08 +0900

    http://jekalmin.tistory.com/entry/spring-bootoauth-%EC%84%B8%ED%8C%85-%E D%85%8C%EC%8A%A4%ED%8A%B8
    해당 url 참고하였으며 mac에서 curl로 테스트가 완료되었습니다

--- api/src/main/java/org/ahea/blindinterview/ApiApplication.java
+++ api/src/main/java/org/ahea/blindinterview/ApiApplication.java
@@ -1,12 +1,122 @@
 package org.ahea.blindinterview;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Blind Interview Server Running\n";
+	}
+
+	@Configuration
+    @EnableResourceServer
+    protected static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                // Just for laughs, apply OAuth protection to only 2 resources
+                .requestMatchers().antMatchers("/","/admin/beans").and()
+                .authorizeRequests()
+                .anyRequest().access("#oauth2.hasScope('read')");
+            // @formatter:on
+        }
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.resourceId("sparklr");
+        }
+
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            // @formatter:off
+            clients.inMemory()
+                .withClient("blind-interview-client")
+                    .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                    .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                    .scopes("read", "write", "trust")
+                    .resourceIds("sparklr")
+                    .accessTokenValiditySeconds(60)
+            .and()
+                .withClient("blind-interview-client-with-registered-redirect")
+                    .authorizedGrantTypes("authorization_code")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read", "trust")
+                    .resourceIds("sparklr")
+                    .redirectUris("http://localhost:8080")
+            .and()
+                .withClient("blind-interview-client-with-secret")
+                    .authorizedGrantTypes("client_credentials", "password")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read")
+                    .resourceIds("sparklr")
+                    .secret("secret");
+        // @formatter:on
+        }
+
+    }
+
+    @Configuration
+    @EnableWebMvcSecurity
+    protected static class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("aHeaUser").password("aHeaPwd").roles("USER");
+        }
+
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean()
+                throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+    }
+
 }
