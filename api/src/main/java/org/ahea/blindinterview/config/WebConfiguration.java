8c2aea77c984be435e7d1b27500f8c82465e9ace
nohsunghyun <nsh1090@brickand.com>
2016-08-04 14:57:22 +0900

    WebMvcConfigurerAdapter를 통한 cros domain mapping 설정

--- api/src/main/java/org/ahea/blindinterview/config/WebConfiguration.java
+++ api/src/main/java/org/ahea/blindinterview/config/WebConfiguration.java
@@ -1,17 +1,22 @@
 package org.ahea.blindinterview.config;
 
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 //	@Bean
 //    ServletRegistrationBean h2servletRegistration(){
 //        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
 ////        registrationBean.addUrlMappings("/console/*");
 //        return registrationBean;
 //    }
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+	
 }
