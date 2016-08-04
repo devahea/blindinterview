6c565dd35437a107c8c0c1419078f89f30223429
nohsunghyun <nsh1090@brickand.com>
2016-08-04 09:37:58 +0900

    SpringBootServletInitializer를 상속 받아야 jenkins에서 war배포시 spring context가 올라갑니다

--- api/src/main/java/org/ahea/blindinterview/ApiApplication.java
+++ api/src/main/java/org/ahea/blindinterview/ApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
