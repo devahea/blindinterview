ae0fa2e9d204b5df794e86395b9277605906b983
nohsunghyun <nsh1090@brickand.com>
2016-08-27 18:34:30 +0900

    ResumeController merge error 처리
    ResumeController merge error 처리

--- api/src/main/java/org/ahea/blindinterview/controller/ResumeController.java
+++ api/src/main/java/org/ahea/blindinterview/controller/ResumeController.java
@@ -1,27 +1,21 @@
 package org.ahea.blindinterview.controller;
 
-<<<<<<< HEAD
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
-=======
->>>>>>> origin/final
 
 import org.ahea.blindinterview.model.resume.Resume;
 import org.ahea.blindinterview.model.resume.ResumeFile;
 import org.ahea.blindinterview.model.resume.ResumeFileRepository;
 import org.ahea.blindinterview.model.resume.ResumeRepository;
-import org.ahea.blindinterview.model.vo.ResponseVO;
 import org.ahea.blindinterview.util.FileWriter;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -79,45 +73,10 @@ public class ResumeController {
 		return "redirect:list";
 	}
 	
-	
 
-
-<<<<<<< HEAD
 	  @RequestMapping(value="delete",method = RequestMethod.POST)
 	  public String delete(String resumeId, Model model) {
 	    resumeRepository.delete(resumeId);
 	    return "redirect:list";
 	  }
-=======
-
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String create(Resume resume, Model model) {
-		resumeRepository.save(resume);
-		model.addAttribute("resume", resume);
-		return "redirect:resume/view?resumeId=" + resume.getId();
-	}
-	
-	@RequestMapping(value = "/view", method = RequestMethod.GET)
-	public String view(String resumeId, Model model) {
-		logger.info("view called..");
-		Resume resume = resumeRepository.findOne(resumeId);
-		model.addAttribute("resume", resume);
-		return "resume/view";
-	}
-
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(Resume resume, Model model) {
-		Resume updateresume = resumeRepository.findOne(resume.getId()); 
-		resumeRepository.save(updateresume);		
-		model.addAttribute("resume", resume);
-		return "redirect:resume/view?resumeId=" + resume.getId();
-	}
-	
-	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-	@ResponseBody
-	public ResponseVO delete(String resumeId, Model model) {
-		resumeRepository.delete(resumeId);
-		return ResponseVO.ok();
-	}
->>>>>>> origin/final
 }
